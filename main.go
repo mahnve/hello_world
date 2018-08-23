54a686d85ed75967b3b01632b0bb8aa058d8b5b8
Marcus Ahnve <marcus@ahnve.com>
2018-08-23 17:50:15 +0200

    Read name and be polite

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("What's your name?")
+	text, _ := reader.ReadString('\n')
+	fmt.Println("Hello " + text)
 }
